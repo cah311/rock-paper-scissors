304bf0595c37aaa9784855d1e7def17169dc5ef5
Carson <c_halter@live.ca>
2022-03-25 06:48:03 -0600

    Test user input

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,7 +1,17 @@
+
+let playerSelection = prompt("Rock, Paper, or Scissors");
+console.log(playerSelection);
+
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
+
+// function game() {
+//     for (let i = 0; i <= 5; i++) {
+//         //code here
+//     }
+// }
