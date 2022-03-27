e0943e8e1054d922d1fe9dda1fb26ea59cb9c427
Carson <c_halter@live.ca>
2022-03-26 23:23:14 -0600

    Test First condition

--- javaScript/index.js
+++ javaScript/index.js
@@ -10,6 +10,25 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == 'Rock') && (computerSelection == 'Scissors')) {
+        
+        let result = ("You Win! Rock beats Scissors");
+        return result; 
+
+    } else if ((playerSelection == 'Rock') && (computerSelection == 'Paper')) {
+        
+        let result = ("You lose! Paper beats Rock");
+        return result; 
+
+
+    } else if (playerSelection == computerSelection) {
+        
+        let result =("It's a tie!");
+        return result;
+
+    }
+
+    
     
 }
 
@@ -17,7 +36,7 @@ function playRound(playerSelection, computerSelection) {
 const computerSelection = computerPlay();
 
 console.log(computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
 
 // function game() {
 //     for (let i = 0; i <= 5; i++) {
