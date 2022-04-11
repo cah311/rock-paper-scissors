1f199e5e6414f64933e0506eabb84674bc497679
Carson <c_halter@live.ca>
2022-04-11 07:18:20 -0600

    Revise button selectors

--- javaScript/index.js
+++ javaScript/index.js
@@ -5,40 +5,91 @@ let playerSelection;
 let computerSelection;
 let round = 0; 
 
-playerSelection = document.querySelectorAll('.btn');
-playerSelection.forEach((button) => {
-    button.addEventListener('click', function () {
-        console.log(button.id);
-        
-        if (button.id === 'rBtn') {
-           
-            playerSelection = 'ROCK'
-            console.log(playerSelection)
-            //return playerSelection
-
-        } else if (button.id === 'pBtn') {
-
-            playerSelection = 'PAPER'
-            console.log(playerSelection)
-            //return playerSelection
-
-        } else if (button.id === 'sBtn') {
-
-            playerSelection = 'SCISSORS'
-            console.log(playerSelection)
-            //return playerSelection
-        }
-
-        return playerSelection
-         
-        
-    });
+playerSelectionR = document.querySelector('#rBtn');
+playerSelectionR.addEventListener('click', function () {
+    playerSelection = "ROCK"
     console.log(playerSelection)
+    computerPlay();
+    if ((playerSelection == 'ROCK') && (computerSelection == 'SCISSORS')) {
+        
+        let result = ("You Win! Rock beats Scissors");
+        playerWins++;
+        console.log(result);
 
-})
+    } else if ((playerSelection == 'ROCK') && (computerSelection == 'PAPER')) {
+        
+        let result = ("You lose! Paper beats Rock");
+        computerWins++; 
+        console.log(result);
 
-   
+    } else if (playerSelection == computerSelection) {
+        
+        let result = ("It's a tie!");
+        console.log(result);
+        
+    }
+    console.log(playerWins);
+    console.log(computerWins);
+    return
 
+});
+
+playerSelectionP = document.querySelector('#pBtn');
+playerSelectionP.addEventListener('click', function () {
+    playerSelection = "PAPER"
+    console.log(playerSelection)
+    computerPlay();
+    if ((playerSelection == 'PAPER') && (computerSelection == 'ROCK')) {
+        
+        let result = ("You Win! Rock beats Scissors");
+        playerWins++;
+        console.log(result);
+
+    } else if ((playerSelection == 'PAPER') && (computerSelection == 'SCISSORS')) {
+        
+        let result = ("You lose! Paper beats Rock");
+        computerWins++; 
+        console.log(result);
+
+    } else if (playerSelection == computerSelection) {
+        
+        let result = ("It's a tie!");
+        console.log(result);
+        
+    }
+    console.log(playerWins);
+    console.log(computerWins);
+    return
+});
+
+playerSelectionS = document.querySelector('#sBtn');
+playerSelectionS.addEventListener('click', function () {
+    playerSelection = "SCISSORS"
+    console.log(playerSelection)
+    computerPlay();
+    if ((playerSelection == 'SCISSORS') && (computerSelection == 'ROCK')) {
+        
+        let result = ("You Win! Rock beats Scissors");
+        playerWins++;
+        console.log(result);
+
+    } else if ((playerSelection == 'SCISSORS') && (computerSelection == 'PAPER')) {
+        
+        let result = ("You lose! Paper beats Rock");
+        computerWins++; 
+        console.log(result);
+
+    } else if (playerSelection == computerSelection) {
+        
+        let result = ("It's a tie!");
+        console.log(result);
+        
+    }
+    console.log(playerWins);
+    console.log(computerWins);
+    return
+});
+    
 
 
 /* Generates random selection for computer */ 
@@ -51,4 +102,8 @@ function computerPlay() {
     return computerPlay; 
     
 }
+
+
+
+
   
